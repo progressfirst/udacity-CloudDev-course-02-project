2251c75c60a254014a562740e3a02f6d8196d3b6
mho <michael.hoeltkemeier@securelink.com>
2020-04-23 00:02:24 +0200

    changes

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -13,19 +13,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get('/filteredimage', async ( req, res ) => {
+    //const caption = req.body.caption;
+    const image_url = req.query.image_url;
+    if (!image_url) {
+      res.status(400).send("Got no image_url !")
+    }
+    let files: string[] = [];
+    let file = await filterImageFromURL(image_url);
+    if (file) {
+      res.status(200).sendFile(file);
+    }
+    else {
+      res.status(400).send("oops");
+    }
+    files.join(file);
+    await deleteLocalFiles(files);
+  } );
+
 
   /**************************************************************************** */
 
